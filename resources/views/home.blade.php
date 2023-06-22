e58a5b50df4a3c4bb3922026ccd21bc5fed0eb86
Paolo96700 <paolo.falcoapp@gmail.com>
2023-06-22 15:48:34 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primo Laravel</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+
+    <ul>
+        <li><a href="/gallery">Galleria</a></li>
+        <li><a href="/contacts">Contatti</a></li>
+    </ul>
+</body>
+</html>
